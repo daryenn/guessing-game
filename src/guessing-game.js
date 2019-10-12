4497285b599e8adfc242b78a663ba3d175389cb3
daryenn <d.antonova96@outlook.com>
2019-10-13 02:01:06 +0300

    feat: solution of guessing game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.result;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.result = Math.floor((this.min+this.max)/2);
     }
 
     lower() {
-
+        this.max = this.result;
     }
 
     greater() {
-
+        this.mix = this.result;
     }
 }
 
